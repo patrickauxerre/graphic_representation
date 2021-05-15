d80802ac7403bbe3353c0b2010e495d78bcf92d7
Patrick Auxerre <patrickauxerre@gmail.com>
2021-05-15 17:56:41 +0200

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -29,8 +29,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,35 +36,38 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            DiscreteGraphic(
-              size: Size(MediaQuery.of(context).size.width,
-                  MediaQuery.of(context).size.height * 0.5),
-              nums: [-1,1, 2, 5, 3, 7, 13, 7,-1],
-              listGradX: [
-                "",
-                "Lun",
-                "Mar",
-                "Mer",
-                "Jeu",
-                "Ven",
-                "Sam",
-                "Dim",
-                ""
-              ],
-              colorAxes: Colors.black,
-              nbGradY: 10,
-              colorBox: Colors.red,
-              colorPoint: Colors.purple,
-              colorLine: Colors.blue,
-              boxWidth: 15.0,
-              minY: 0,
-              maxY: 20,
-            ),
-            Text("COUCOU")
-          ],
-        ),
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+              padding: EdgeInsets.only(top: 20.0, bottom: 10.0),
+              child: Text(
+                "PRECIPITATIONS (mm)",
+                style: TextStyle(fontWeight: FontWeight.bold),
+                textScaleFactor: 1.6,
+              )),
+          DiscreteGraphic(
+            size: Size(MediaQuery.of(context).size.width,
+                MediaQuery.of(context).size.height * 0.5),
+            nums: [1, 2, 5, 3, 7, 13, 7],
+            listGradX: [
+              "Lun",
+              "Mar",
+              "Mer",
+              "Jeu",
+              "Ven",
+              "Sam",
+              "Dim",
+            ],
+            colorAxes: Colors.black,
+            nbGradY: 10,
+            colorPoint: Colors.blue,
+            colorLine: Colors.blue,
+            boxWidth: 10.0,
+            minY: 0,
+            maxY: 20,
+          ),
+        ],
+      ),
     );
   }
 }
