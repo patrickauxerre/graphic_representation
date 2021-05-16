37b10c6c52ba2032b9965bea1b6b3a21bb81dd76
Patrick Auxerre <patrickauxerre@gmail.com>
2021-05-16 09:19:46 +0200

    comment code

--- lib/graphic_representation.dart
+++ lib/graphic_representation.dart
@@ -30,12 +30,18 @@ class _GraphCustomPainter extends CustomPainter {
   Color? colorBox;
   double? boxWidth;
 
+  ///override paint : pour dessiner les axes, points, lignes suivant les valeur contenues dans listNum
   @override
   void paint(Canvas canvas, Size size) {
+    ///Amplitude sur l'axe vertical
     var delta = maxY - minY;
+    ///Valeur du strokeWidth utilisé pour les barres verticales
     double bw = (boxWidth == null) ? 5 : boxWidth!;
+    ///Draw the entire sequence of point as one line.
     final pointMode = ui.PointMode.polygon;
+    ///Hauteur d'une graduation verticale
     double gradVertical = size.height / (nbGradY - 1);
+    ///Création des différents points puis tracé des lignes verticales avec canvas.drawPoints
     for (int i = 0; i <= nbGradY - 1; i++) {
       var points1 = [
         Offset(0, i * gradVertical.toDouble()),
@@ -47,7 +53,9 @@ class _GraphCustomPainter extends CustomPainter {
         ..strokeCap = StrokeCap.round;
       canvas.drawPoints(pointMode, points1, paint1);
     }
+    ///Largeur d'une graduation horizontale
     double gradHorizontal = size.width / (nbGradX - 1);
+    ///Création des différents points puis tracé des lignes horizontales avec canvas.drawPoints
     for (int i = 0; i <= nbGradX - 1; i++) {
       var points1 = [
         Offset(i * gradHorizontal.toDouble(), 0),
@@ -58,43 +66,50 @@ class _GraphCustomPainter extends CustomPainter {
         ..strokeWidth = 1
         ..strokeCap = StrokeCap.round;
       canvas.drawPoints(pointMode, points1, paint1);
-      int index = 0;
-      var stepX = size.width / (listNum.length - 1);
-      List<Offset> offSets = [];
-      listNum.forEach((element) {
-        offSets.add(Offset(index * stepX,
-            size.height - ((element - minY) / delta) * size.height));
-        index++;
-      });
-      for (int i = 0; i < offSets.length - 1; i++) {
-        if (colorLine != null) {
-          var paint1 = Paint()
-            ..color = colorLine!
-            ..strokeWidth = 3
-            ..strokeCap = StrokeCap.round;
-          if (offSets[i].dy >= 0 &&
-              offSets[i].dy <= size.height &&
-              offSets[i + 1].dy >= 0 &&
-              offSets[i + 1].dy <= size.height) {
-            canvas.drawPoints(pointMode, [offSets[i], offSets[i + 1]], paint1);
-          }
+    }
+    /// index de parcours dans listNum
+    int index = 0;
+    /// Largeur entre deux points
+    var stepX = size.width / (listNum.length - 1);
+    /// Liste des points correspondant à listNum
+    List<Offset> offSets = [];
+    /// Ajout des points
+    listNum.forEach((element) {
+      offSets.add(Offset(index * stepX,
+          size.height - ((element - minY) / delta) * size.height));
+      index++;
+    });
+    /// Tracé des lignes si colorLine != null
+    for (int i = 0; i < offSets.length - 1; i++) {
+      if (colorLine != null) {
+        var paint1 = Paint()
+          ..color = colorLine!
+          ..strokeWidth = 3
+          ..strokeCap = StrokeCap.round;
+        if (offSets[i].dy >= 0 &&
+            offSets[i].dy <= size.height &&
+            offSets[i + 1].dy >= 0 &&
+            offSets[i + 1].dy <= size.height) {
+          canvas.drawPoints(pointMode, [offSets[i], offSets[i + 1]], paint1);
         }
       }
-      for (int i = 0; i <= offSets.length - 1; i++) {
-        if (colorPoint != null &&
-            offSets[i].dy >= 0 &&
-            offSets[i].dy <= size.height) {
-          var paint2 = Paint()..color = colorPoint!;
-          canvas.drawCircle(offSets[i], 4, paint2);
-        }
-        if (colorBox != null) {
-          var paint3 = Paint()
-            ..color = colorBox!
-            ..strokeWidth = bw;
-          if (offSets[i].dy >= 0 && offSets[i].dy <= size.height) {
-            canvas.drawPoints(pointMode,
-                [offSets[i], Offset(offSets[i].dx, size.height)], paint3);
-          }
+    }
+    /// Tracé des points si colorPoint != null
+    /// Tracé des barres verticales si colorBox != null
+    for (int i = 0; i <= offSets.length - 1; i++) {
+      if (colorPoint != null &&
+          offSets[i].dy >= 0 &&
+          offSets[i].dy <= size.height) {
+        var paint2 = Paint()..color = colorPoint!;
+        canvas.drawCircle(offSets[i], 4, paint2);
+      }
+      if (colorBox != null) {
+        var paint3 = Paint()
+          ..color = colorBox!
+          ..strokeWidth = bw;
+        if (offSets[i].dy >= 0 && offSets[i].dy <= size.height) {
+          canvas.drawPoints(pointMode,
+              [offSets[i], Offset(offSets[i].dx, size.height)], paint3);
         }
       }
     }
